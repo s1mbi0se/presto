87310ab9e20048837f0269e004ec36cdba7a9156
Caio Pavanelli <caiopava@gmail.com>
2020-02-04 20:37:47 -0300

    Feature/dynamic catalog (#1)
    Adds a functionality to load catalogs from an API call.
    
    This PR will add a DynamicCatalogStore class which will be responsible for sending a GET request to an internal API and retrieve available catalog data on demand. From time to time, Presto will also send a GET request to get the delta of updated catalogs since last request.
    
    * add data connection test payload
    
    * add dynamic catalog store
    
    * add data connection response
    
    * adds data connection response object and tests the funcionality of retriving data from external api
    
    * implements dynamic catalog store initial api configuration
    
    * add api configuration file
    
    * add dynamic catalog store files
    
    * add explicit binder for DynamicCatalogStore
    
    * call DynamicCatalogStore on Presto server load
    
    * change sample config file to port 8081 - conflicts with api on 8080
    
    * add catalog delta retriever scheduler
    
    * add data connections delta update
    
    * add unit tests to dynamic catalog store
    
    * fix require non null checks

---
+++ presto-main/src/main/java/io/prestosql/metadata/DataConnectionParser.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.metadata;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
+
+import static com.google.common.base.Preconditions.checkState;
+
+public class DataConnectionParser
+{
+    public static Map<String, String> getCatalogProperties(String connectorName, Map<String, String> dataConnectionsProperties)
+    {
+        ImmutableMap.Builder<String, String> catalog = ImmutableMap.builder();
+
+        String connectionUrl = getJdbcConnectionString(connectorName, dataConnectionsProperties);
+        catalog.put("connection-url", connectionUrl);
+        catalog.put("connection-user", dataConnectionsProperties.get("username"));
+        catalog.put("connection-password", dataConnectionsProperties.get("password"));
+
+        return catalog.build();
+    }
+
+    private DataConnectionParser() {}
+
+    private static String getJdbcConnectionString(String connectorName, Map<String, String> dataConnectionsProperties)
+    {
+        String jdbc = null;
+        switch (connectorName) {
+            case "postgres":
+                jdbc = String.format("jdbc:%s://%s:%s", connectorName, dataConnectionsProperties.get("host"), dataConnectionsProperties.get("host-port"),
+                        dataConnectionsProperties.get("database-name"));
+                break;
+            case "mysql":
+            default:
+                jdbc = String.format("jdbc:%s://%s:%s", connectorName, dataConnectionsProperties.get("host"), dataConnectionsProperties.get("host-port"));
+        }
+
+        checkState(jdbc != null, "Catalog configuration %s cannot infer connector-url", connectorName);
+
+        return jdbc;
+    }
+}
