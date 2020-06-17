48636318ac90af59f349b5b224d65f031d762f2e
caio <caiopava@gmail.com>
2020-06-17 15:00:59 -0300

    feat: aspect logging on presto-main

---
+++ presto-main/src/main/java/io/prestosql/logging/aspects/BaseAspect.java
@@ -0,0 +1,98 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.logging.aspects;
+
+import io.airlift.log.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+
+import java.util.Map;
+
+@Aspect
+public abstract class BaseAspect
+{
+    private static final Logger log = Logger.get(BaseAspect.class);
+
+//    protected static final int NODE_ID = SDBConfig.getIntValue("S1S_NODE_ID", 0);
+//    protected static final String DATACENTER_NAME = SDBInstanceSettings.get().getDataCenterName();
+//    protected static final String NODE_HOSTNAME = getNodeHostname();
+//
+//    private static String getNodeHostname() {
+//        final String[] shannondbHosts = SDBConfig.getValues("shannondb.nodes");
+//
+//        if (shannondbHosts == null || shannondbHosts.length <= NODE_ID) {
+//            return "localhost";
+//        }
+//
+//        final String completePath = shannondbHosts[NODE_ID];
+//
+//        final int separatorIndex = completePath.lastIndexOf(':');
+//        // this class get host and port to send message from properties file
+//        return separatorIndex > 0 ? completePath.substring(0, separatorIndex) : completePath;
+//    }
+
+    protected Object printDebugLogForMethod(final ProceedingJoinPoint point, final long threadId)
+            throws Throwable
+    {
+        final long start = System.currentTimeMillis();
+
+        Object result = null;
+
+        try {
+            result = point.proceed();
+        }
+        catch (Throwable throwable) {
+            if (throwable instanceof Exception) {
+                final Class<? extends Throwable> exceptionClass = throwable.getClass();
+
+                throw exceptionClass.cast(throwable);
+            }
+            throw throwable;
+        }
+        final long timeToExecuteMethod = System.currentTimeMillis() - start;
+
+        final Integer debugStepId = 0; //getThreadIdToStep().get(threadId);
+        final Long debugLogId = 0L; //getThreadIdToDebugLogId().get(threadId);
+        if (debugLogId != null && debugStepId != null) {
+            final MethodSignature methodAttached = (MethodSignature) point.getSignature();
+            final String methodName = methodAttached.getMethod().getName();
+
+            final String debugLogMessage =
+                    String.format(
+                            "%s - %d - %d - %d - %s - %s  - %s - %s",
+                            getFlowName(),
+                            debugLogId,
+                            debugStepId,
+                            timeToExecuteMethod,
+                            methodName,
+                            "",
+                            "",
+                            "");
+
+            log.debug(debugLogMessage);
+            System.out.println(debugLogMessage);
+
+            getThreadIdToStep().put(threadId, debugStepId + 1);
+        }
+
+        return result;
+    }
+
+    protected abstract String getFlowName();
+
+    protected abstract Map<Long, Integer> getThreadIdToStep();
+
+    protected abstract Map<Long, Long> getThreadIdToDebugLogId();
+}
