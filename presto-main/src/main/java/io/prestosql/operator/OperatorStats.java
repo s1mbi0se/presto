6eb57b75b688ae8326a15f2e6d0170c934c63c50
Anthony Louis <anthony@simbioseventures.com>
2020-10-07 16:27:57 -0300

    docs: add javadoc for getSumSquaredInputPositions in OperatorStats

--- presto-main/src/main/java/io/prestosql/operator/OperatorStats.java
+++ presto-main/src/main/java/io/prestosql/operator/OperatorStats.java
@@ -290,6 +290,11 @@ public class OperatorStats
         return inputPositions;
     }
 
+    /**
+     * Gets the sum of the square of all input positions.
+     *
+     * @return the sum of the square of all input positions
+     */
     @JsonProperty
     public double getSumSquaredInputPositions()
     {
