49fa1a82073d2ade10411a8c09b403f1401a67b2
Anthony Louis <anthony@simbioseventures.com>
2020-10-09 15:04:10 -0300

    docs: add javadoc for getLastStartTime in TaskStats

--- presto-main/src/main/java/io/prestosql/operator/TaskStats.java
+++ presto-main/src/main/java/io/prestosql/operator/TaskStats.java
@@ -258,6 +258,11 @@ public class TaskStats
         return firstStartTime;
     }
 
+    /**
+     * Gets the time that the task started.
+     *
+     * @return the time that the task started
+     */
     @Nullable
     @JsonProperty
     public DateTime getLastStartTime()
